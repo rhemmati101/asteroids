84446db51d2779dccf30d26054640de8f190e28d
rhemmati101 <rhemmati@g.ucla.edu>
2024-09-30 14:23:25 -0700

    implemented asteroid field

--- asteroid.py
+++ asteroid.py
@@ -8,3 +8,6 @@ class Asteroid(CircleShape):
     
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
