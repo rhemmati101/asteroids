493f1d1df8e2921f088f529ae4f85b9f9002fc20
rhemmati101 <rhemmati@g.ucla.edu>
2024-09-29 19:27:41 -0700

    implement screen

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
-import constants
+from constants import *
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        # checks if window is open
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
 
 
 
